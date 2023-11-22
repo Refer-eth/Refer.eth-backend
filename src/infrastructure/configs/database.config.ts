471c6677dae81ae422e1d4fa0937b4ac1a6f669c
Ali Ebrahimi <a.ebrahimi@wallex.net>
2023-11-22 13:21:40 +0330

    change database host

--- src/infrastructure/configs/database.config.ts
+++ src/infrastructure/configs/database.config.ts
@@ -2,7 +2,7 @@ const production = {
   username: 'root',
   password: '1234',
   database: 'refer_eth',
-  host: 'localhost',
+  host: '0.0.0.0',
   dialect: 'mysql',
   port: 3306,
   dialectOptions: {
